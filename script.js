cff91c0bf78ec8a6c85ac34f622a32281556069f
jca26c <78522594+jca26c@users.noreply.github.com>
2021-02-08 20:24:59 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var map = L.map('map').setView([37.7, -122.4], 10);
 
 var Stamen_Terrain = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/terrain/{z}/{x}/{y}.{ext}', {
-	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	subdomains: 'abcd',
 	minZoom: 0,
-	maxZoom: 18,
+	maxZoom: 20,
 	ext: 'png'
 }).addTo(map);
 
